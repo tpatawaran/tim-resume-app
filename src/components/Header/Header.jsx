0c2b87fd6d4b67839222a8a806a9ea66ccfe3aae
Timothy Patawaran <tpatawaran@outlook.com>
2024-06-03 20:29:41 -0700

    third commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.scss";
 import menu from "../../assets/Icons/hamburger_menu_icon.svg";
 import close from "../../assets/Icons/close_hamburger_menu_icon.svg";
+import logo from "../../assets/Icons/logo_icon.svg";
 import { useState } from "react";
 
 function Header() {
@@ -14,8 +15,11 @@ function Header() {
     return (
         <>
             <nav className="nav">
+                <div className="header_wrapper">
+                <img src={logo} alt="logo" className="logo" />
                 <img src={menu} onClick={ToggleMenu} alt="Hamburger Menu" className={` ${menuOpen ? "menu" : "menu_alt"}`} />
                 <img src={close} onClick={ToggleMenu} alt="Hamburger Menu" className={` ${menuOpen ? "close_alt" : "close"}`} />
+                </div>
                 <header className={`header ${menuOpen ? "active" : ""}`}>
 
                     <ul className={`header_list ${menuOpen ? "" : "active"}`}>
@@ -37,23 +41,24 @@ function Header() {
                 </header>
             </nav>
             <nav className="nav_desktop">
-                <ul className="nav_desktop_list">
-                    <li className="header_list_item">
-                        <a href="#About">About</a>
-                    </li>
-                    <li className="header_list_item">
-                        <a href="#Education">Education</a>
-                    </li>
-                    <li className="header_list_item">
-                        <a href="#Technical_Skills">Technical Skills</a>
-                    </li>
-                    <li className="header_list_item">
-                        <a href="#Work_Experience">Work Experience</a>
-                    </li>
-                    <li className="header_list_item">
-                        <a href="#Contact">Contact</a>
-                    </li>
-                </ul>
+                    <img src={logo} alt="logo" className="logo_desktop" />
+                    <ul className="nav_desktop_list">
+                        <li className="header_list_item">
+                            <a href="#About">About</a>
+                        </li>
+                        <li className="header_list_item">
+                            <a href="#Education">Education</a>
+                        </li>
+                        <li className="header_list_item">
+                            <a href="#Technical_Skills">Technical Skills</a>
+                        </li>
+                        <li className="header_list_item">
+                            <a href="#Work_Experience">Work Experience</a>
+                        </li>
+                        <li className="header_list_item">
+                            <a href="#Contact">Contact</a>
+                        </li>
+                    </ul>
             </nav>
         </>
     )
--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -1,16 +1,16 @@
 import "./Sections.scss";
 
 function Sections() {
-    // test
     return (
         <>
-            <section  id="About" className="About">
+            <section className="hero" >
+                <h3 className="hero_subheading">Hello! I'm</h3>
+                <h1 className="hero_heading">Timothy Patawaran</h1>
+                <h2 className="hero_statement">IT Student on a mission</h2>
+            </section>
+            <section id="About" className="About">
                 <h2 className="section_title">About</h2>
                 <p className="section_information">
-                    Hello! I'm
-                    <br></br>
-                    Timothy Patawaran
-                    <br></br>
                     Bachelor of Technology fourth year student with six years of experience in sales and customer service. An exceptional listener and effective communicator in verbal and written language. Excellent analytical, time management and problem-solving skills. Highly organized with the ability to manage multiple projects and consistently meet deadlines.
                 </p>
             </section>
@@ -22,36 +22,36 @@ function Sections() {
             </section>
             <section id="Technical_Skills" className="Technical_Skills">
                 <h2 className="section_title">Technical Skills</h2>
-                <p className="section_information">
-                    ●	Extensive knowledge with the Software Development Life Cycle and Project Management process
-                    ●	Excellent troubleshooting skills and able to resolve unfamiliar issues through research
-                    ●	Programming Languages: 	C++, Java, HTML, CSS, Python, PHP, SQL
-                    ●	Operating Systems:		Windows, MacOS, Linux
-                    ●	Software:			Microsoft 365, Google Drive, iCloud, Oracle VM VirtualBox, Packet Tracer, Wireshark
-                </p>
+                <ul className="section_information">
+                    <li className="section_information">Extensive knowledge with the Software Development Life Cycle and Project Management process</li>
+                    <li className="section_information">Excellent troubleshooting skills and able to resolve unfamiliar issues through research</li>
+                    <li className="section_information">Programming Languages: 	C++, Java, HTML, CSS, Python, PHP, SQL</li>
+                    <li className="section_information">Operating Systems:		Windows, MacOS, Linux</li>
+                    <li className="section_information">Software:			Microsoft 365, Google Drive, iCloud, Oracle VM VirtualBox, Packet Tracer, Wireshark</li>
+                </ul>
             </section>
             <section id="Work_Experience" className="Work_Experience">
-            <h2 className="section_title">Work Experience</h2>
-            <p className="section_information">
-                Junior Network & I.T Technician (Co-op) | Delta Police Department | September 2023 – Present
-                ●	Collaborated in the creation of the organization’s SharePoint site, enhancing communication and information sharing among team members
-                ●	Provided daily user and desktop support, resolving issues promptly and efficiently
-                ●	Developed and implemented various Microsoft Power Automations to streamline processes and increase efficiency across different departments
-                ●	Supported local Community Police Offices by managing their phones and computers while addressing technical concerns
-                ●	Assisted the Network Administrator with server support and administration tasks, contributing to network stability
-                ●	Efficiently installed, troubleshooted, and supported Mobile Data Terminals, ensuring seamless operation
+                <h2 className="section_title">Work Experience</h2>
+                <p className="section_information">
+                    Junior Network & I.T Technician (Co-op) | Delta Police Department | September 2023 – Present
+                    ●	Collaborated in the creation of the organization’s SharePoint site, enhancing communication and information sharing among team members
+                    ●	Provided daily user and desktop support, resolving issues promptly and efficiently
+                    ●	Developed and implemented various Microsoft Power Automations to streamline processes and increase efficiency across different departments
+                    ●	Supported local Community Police Offices by managing their phones and computers while addressing technical concerns
+                    ●	Assisted the Network Administrator with server support and administration tasks, contributing to network stability
+                    ●	Efficiently installed, troubleshooted, and supported Mobile Data Terminals, ensuring seamless operation
 
-                Deployment Technician (Co-op) | Microserve | January 2022 - September 2022
-                ●	Efficiently supported IT hardware and software at various client sites, including, Fraser Health Authority, WorkSafeBC, Provincial Health Services Authority, Vancouver Island Health Authority, and Interior Health Authority
-                ●	Effectively configured hardware, software, and networks, while troubleshooting any problems in a team environment
-                ●	Provided on-site client training for newly deployed hardware and software environments
-                ●	Responded to end-user inquiries and provided technical support to clients
-                ●	Accurately trained and guided new employees following company protocols
-            </p>
+                    Deployment Technician (Co-op) | Microserve | January 2022 - September 2022
+                    ●	Efficiently supported IT hardware and software at various client sites, including, Fraser Health Authority, WorkSafeBC, Provincial Health Services Authority, Vancouver Island Health Authority, and Interior Health Authority
+                    ●	Effectively configured hardware, software, and networks, while troubleshooting any problems in a team environment
+                    ●	Provided on-site client training for newly deployed hardware and software environments
+                    ●	Responded to end-user inquiries and provided technical support to clients
+                    ●	Accurately trained and guided new employees following company protocols
+                </p>
             </section>
             <section id="Contact" className="Contact">
-            <h2 className="section_title">Contact</h2>
-            <p className="section_information"></p>
+                <h2 className="section_title">Contact</h2>
+                <p className="section_information"></p>
             </section>
         </>
     )
